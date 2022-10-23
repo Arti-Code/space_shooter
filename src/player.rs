b7aba9e9ca684fc9ea2795b0c70e4c7fa46fdaba
Arturo <41492101+Arti-Code@users.noreply.github.com>
2022-10-23 05:34:09 +0200

    init

---
+++ src/player.rs
@@ -0,0 +1,62 @@
+use std::sync::Arc;
+use bevy::prelude::*;
+use crate::{GameTextures, WinSize, PLAYER_SIZE, SPRITE_SCALE, TIME_STEP, BASE_SPEED};
+use crate::components::{Player, Velocity, Movable};
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system_to_stage(StartupStage::PostStartup, player_spawn_system)
+        //.add_system(player_movement_system)
+        .add_system(player_keyboard_event_system)
+        .add_system(player_fire_system);
+    }
+}
+
+fn player_spawn_system(mut commands: Commands, game_textures: Res<GameTextures>, win_size: Res<WinSize>,) {
+    let bottom = -win_size.h/2.;
+    commands.spawn_bundle(SpriteBundle {
+        texture: game_textures.player.clone(),
+        transform: Transform {
+            translation: Vec3::new(0., bottom+PLAYER_SIZE.1 / 2. * SPRITE_SCALE + 5., 10.),
+            scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .insert(Player)
+    .insert(Movable {auto_despawn: false})
+    .insert(Velocity {x: 0.0, y: 0.0});
+}
+
+fn player_keyboard_event_system(kb: Res<Input<KeyCode>>, mut query: Query<&mut Velocity, With<Player>>,) {
+    if let Ok(mut velocity) = query.get_single_mut() {
+        velocity.x = if kb.pressed(KeyCode::Left) {
+            -1.
+        } else if kb.pressed(KeyCode::Right) {
+            1.
+        } else {
+            0.
+        }
+    }
+}
+
+fn player_fire_system(mut commands: Commands, kb: Res<Input<KeyCode>>, game_textures: Res<GameTextures>, query: Query<&Transform, With<Player>>,) {
+    if let Ok(player_tf) = query.get_single() {
+        if kb.just_pressed(KeyCode::Space) {
+            let (x, y) = (player_tf.translation.x, player_tf.translation.y);
+            commands.spawn_bundle(SpriteBundle {
+                texture: game_textures.player_laser.clone(),
+                transform: Transform {
+                    translation: Vec3::new(x, y, 0.0),
+                    scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.0),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(Movable {auto_despawn: true})
+            .insert(Velocity {x: 0.0, y: 1.0});
+        }
+    }
+}
